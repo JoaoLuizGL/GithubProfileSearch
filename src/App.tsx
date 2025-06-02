25ecf531566a7f4d35b130ad4ec4c5a8e8b40f33
JoaoLuizGL <joaoluiz.glira@gmail.com>
2025-06-02 12:22:36 -0300

    feat: ícone do Github

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import classes from './App.module.css'
 function App() {
   return (
     <div className={classes.App}>
+      <img src="/github.svg" alt="GitHub Logo" width={32} height={32}/>
       <h1>GitHub Profile Search</h1>
       <Outlet />
     </div>
