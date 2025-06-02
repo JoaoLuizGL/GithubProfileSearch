6631ba38f30d8e98f1a1f4aa8d57d16260351da0
JoaoLuizGL <joaoluiz.glira@gmail.com>
2025-06-02 11:57:22 -0300

    feat: Abre o código do repositório em outra tela

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -2,7 +2,6 @@ import type { ReposProps } from '../types/Repos';
 import { MdCode } from 'react-icons/md';
 import {MdStar} from 'react-icons/md';
 import { MdForkRight } from 'react-icons/md';
-import { Link } from 'react-router-dom';
 import classes from './Repos.module.css';
 
 const Repos = ({name, html_url, language, forks, stargazers_count}:ReposProps) => {
@@ -27,7 +26,7 @@ const Repos = ({name, html_url, language, forks, stargazers_count}:ReposProps) =
                     <p className={classes.number}>{stargazers_count}</p>
                 </div>
             </div>
-            <Link to={html_url} className=''>Ver código</Link>
+            <a href={html_url} target="_blank" rel="noopener noreferrer">Ver código</a>
         </div>
     );
 };
